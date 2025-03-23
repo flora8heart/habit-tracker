a01ca01e183856f79a29a56d50096a67e2971a77
flora8heart <9201125+flora8heart@users.noreply.github.com>
2025-03-23 14:23:49 +0000

    Add switch statement

--- Program.cs
+++ Program.cs
@@ -21,18 +21,42 @@ using (var connection = new SqliteConnection(connectionString))
 
 static void GetUserInput()
 {
-  Console.Clear()
+  Console.Clear();
   bool closeApp = false;
   while (closeApp == false)
   {
     Console.WriteLine("\n\nMAIN MENU");
     Console.WriteLine("\nWhat would you like to do?");
-    Console.WriteLine("\nType 0 to Close Application.");
-    Console.WriteLine("Type 1 to View All Records.");
-    Console.WriteLine("Type 2 to Insert Record.");
-    Console.WriteLine("Type 3 to Delete Record");
-    Console.WriteLine("Type 4 to Update Record");
+    Console.WriteLine("\nType q to Close Application.");
+    Console.WriteLine("Type v to View All Records.");
+    Console.WriteLine("Type i to Insert Record.");
+    Console.WriteLine("Type d to Delete Record");
+    Console.WriteLine("Type u to Update Record");
     Console.WriteLine("--------------------------------------\n");
+
+    string userInput = Console.ReadLine();
+
+    switch (userInput)
+    {
+      case "q":
+        Console.WriteLine("\nGoodbye!\n");
+        closeApp = true;
+        break;
+      case "v":
+        // view all Records
+        ViewAllRecords();
+        break;
+      case "i":
+        // insert Record
+        Insert();
+        break;
+      case "u":
+        // update Record
+        Update();
+        break;
+      default:
+        Console.WriteLine("Invalid Command. Please type q, v, i or u\n")
+    }
   }
 }
 
