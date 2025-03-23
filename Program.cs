94a1c0e7e2dfc67881d76d148acff5f7638614d6
flora8heart <9201125+flora8heart@users.noreply.github.com>
2025-03-23 17:48:11 +0000

    Add delete method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data;
+using System.Data.Common;
+using System.Globalization;
 using Microsoft.Data.Sqlite;
 
 string connectionString = @"Data Source =habit-Tracker.db";
@@ -45,13 +47,17 @@ void GetUserInput()
       //   Console.WriteLine("\nGoodbye!\n");
       //   closeApp = true;
       //   break;
-      // case "v":
-      //   // view all Records
-      //   ViewAllRecords();
-      //   break;
+      case "v":
+        // view all Records
+        ViewAllRecords();
+        break;
       case "i":
         // insert Record
         Insert();
+        break;
+      case "d":
+        // delete record
+        Delete();
         break;
         // case "u":
         //   // update Record
@@ -72,22 +78,20 @@ void Insert()
   int quantity = GetNumberInput("\n\nPlease insert number of glasses(no decimals allowed)\n\n");
 
   // insert into database
-  using (var connection = new SqliteConnection(connectionString))
-  {
-    connection.Open();
-    var sqlCmd = connection.CreateCommand();
-    sqlCmd.CommandText =
-      @"INSERT INTO drinking_water(date, quantity)
-      VALUES(@date, @quantity
-      )";
+  using var connection = new SqliteConnection(connectionString);
+  connection.Open();
 
-    // prevent sql injection
-    sqlCmd.Parameters.AddWithValue("@date", date);
-    sqlCmd.Parameters.AddWithValue("@quantity", quantity);
+  var sqlCmd = connection.CreateCommand();
+  sqlCmd.CommandText =
+    @"INSERT INTO drinking_water(date, quantity)
+    VALUES(@date, @quantity
+    )";
 
-    sqlCmd.ExecuteNonQuery();
-  }
+  // prevent sql injection
+  sqlCmd.Parameters.AddWithValue("@date", date);
+  sqlCmd.Parameters.AddWithValue("@quantity", quantity);
 
+  sqlCmd.ExecuteNonQuery();
 
 }
 
@@ -119,17 +123,76 @@ int GetNumberInput(string message)
   return finalInput;
 }
 
-// void ViewAllRecords()
-// {
-//   Console.Clear();
-//   using (var connection = new SqliteConnection(connectionString))
-//   {
-//     connection.Open();
-//     var sqlCmd = connection.CreateCommand();
+void ViewAllRecords()
+{
+  Console.Clear();
+  using (var connection = new SqliteConnection(connectionString))
+  {
+    connection.Open();
+    var sqlCmd = connection.CreateCommand();
 
-//     sqlCmd.CommandText =
-//       "SELECT * FROM drinking_water";
+    sqlCmd.CommandText =
+      "SELECT * FROM drinking_water";
 
-//     sqlCmd.ExecuteNonQuery();
-//   }
-// }
+    //create new list with the newly created DrinkingWater class
+    List<DrinkingWater> habitData = new();
+
+    SqliteDataReader reader = sqlCmd.ExecuteReader();
+
+    if (reader.HasRows)
+
+    {
+      // add database data to a list
+      while (reader.Read())
+      {
+        habitData.Add(
+        new DrinkingWater
+        {
+          Id = reader.GetInt32(0), //from first column
+          Date = DateTime.ParseExact(reader.GetString(1), "dd-MM-yy", new CultureInfo("en-Us")), // from second column
+          Quantity = reader.GetInt32(2)
+        });
+      }
+
+      // print out list
+      Console.WriteLine("--------------------------------------\n");
+      foreach (var data in habitData)
+      {
+        Console.WriteLine($"{data.Id} -- {data.Date.ToString("dd-MMM-yyyy")} - Quantity: {data.Quantity}");
+      }
+      Console.WriteLine("--------------------------------------\n");
+    }
+    else
+    {
+      Console.WriteLine("\nNo rows found\n");
+    }
+  }
+
+
+}
+
+void Delete()
+{
+  Console.Clear();
+  ViewAllRecords();
+
+  var recordId = GetNumberInput("\n\nPlease type the Id of the record you want to delete or type q to exit to Main Menu\n\n");
+
+  using var connection = new SqliteConnection(connectionString);
+  connection.Open();
+
+  var sql = "DELETE FROM drinking_water WHERE Id = @Id";
+
+  using var command = new SqliteCommand(sql, connection);
+  command.Parameters.AddWithValue("@Id", recordId);
+
+  // execute the INSERT statement
+  var rowInserted = command.ExecuteNonQuery();
+}
+
+class DrinkingWater
+{
+  public int Id { get; set; }
+  public DateTime Date { get; set; }
+  public int Quantity { get; set; }
+}
